aba982b8bcf165c6eaae446ae33c5699d4dd04ba
Mikael Mortensen <mikaem@math.uio.no>
2016-09-08 12:19:21 +0200

    Trying conda-forge for boost and eigen

--- build_fenics_deps.sh
+++ build_fenics_deps.sh
@@ -2,23 +2,23 @@
 
 if [ ${CONDA_BUILD_TYPE} = host-gcc ]; then
     # Dependencies
-    conda build boost/host-gcc
+#    conda build boost/host-gcc
     conda build hdf5-parallel/host-gcc
     conda build h5py-parallel/host-gcc
     conda build vtk/host-gcc
-    conda build eigen3/host-gcc
+#    conda build eigen3/host-gcc
     conda build petsc/host-gcc
     conda build petsc4py/host-gcc
     conda build slepc/host-gcc
 
 else
     # Dependencies
-    conda build boost/conda-gcc
+#    conda build boost/conda-gcc
 #     conda build vtk/conda-gcc
     conda build hdf5-parallel/conda-gcc
     conda build h5py-parallel/conda-gcc
     conda build hwloc/conda-gcc
-    conda build eigen3/conda-gcc
+#    conda build eigen3/conda-gcc
     conda build petsc/conda-gcc
     conda build petsc4py/conda-gcc
     conda build slepc/conda-gcc
