c20777b9e8b0c1d998e37813bd45e52f4509b0bf
Mikael Mortensen <mikaem@math.uio.no>
2016-12-12 09:18:33 +0100

    Remove warnings hdf5

--- hdf5-parallel/conda-gcc/build.sh
+++ hdf5-parallel/conda-gcc/build.sh
@@ -2,8 +2,9 @@
 
 # ln -s $PREFIX/bin/gfortran $PREFIX/bin/f95
 export CC=mpicc
-./configure --prefix=$PREFIX --enable-linux-lfs --with-default-api-version=v18 --enable-unsupported --enable-production=yes --with-pthread --with-zlib=$PREFIX --with-ssl --enable-parallel --enable-shared
+export CFLAGS="-w"
+./configure --prefix=$PREFIX --enable-linux-lfs --with-default-api-version=v18 --enable-unsupported --enable-production=yes --with-pthread --with-zlib=$PREFIX --enable-parallel --enable-shared
 make -j${CPU_COUNT}
 make install
 
-#rm -rf $PREFIX/share/hdf5_examples
+rm -rf $PREFIX/share/hdf5_examples
