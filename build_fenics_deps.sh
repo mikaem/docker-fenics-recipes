a5672889f69a6127efd7f9b972d9d8fa96e7db3b
Mikael Mortensen <mikaem@math.uio.no>
2016-12-12 18:03:57 +0100

    More osx stuff

--- build_fenics_deps.sh
+++ build_fenics_deps.sh
@@ -2,22 +2,31 @@
 
 if [ ${CONDA_BUILD_TYPE} = host-gcc ]; then
     # Dependencies
-#     conda build hdf5-parallel/host-gcc
-#     conda build h5py-parallel/host-gcc
-#    conda build vtk/host-gcc
-#     conda build petsc/host-gcc
-#    conda build petsc4py/host-gcc
-#     conda build slepc/host-gcc
-#     conda build subprocess32/host-gcc
-#     conda build eigen3/host-gcc
+    conda build hdf5-parallel/host-gcc
+    conda build h5py-parallel/host-gcc
+    conda build vtk/host-gcc
+    conda build petsc/host-gcc
+    conda build petsc4py/host-gcc
+    conda build slepc/host-gcc
+    conda build subprocess32/host-gcc
+    conda build eigen3/host-gcc
     conda build boost/host-gcc
 
-else
-    # Dependencies (Cannot build VTK with conda gcc)
+elif [ ${CONDA_BUILD_TYPE} = conda-gcc ]; then
+    # Cannot seem to build VTK with conda gcc
     conda build hdf5-parallel/conda-gcc
     conda build h5py-parallel/conda-gcc
     conda build petsc/conda-gcc
     conda build petsc4py/conda-gcc
     conda build slepc/conda-gcc
+
+else
+    conda build hdf5-parallel/osx-host
+    conda build h5py-parallel/osx-host
+    conda build vtk/osx-host
+    conda build petsc/osx-host
+    conda build petsc4py/osx-host
+    conda build slepc/osx-host
+    conda build slepc4py/osx-host
 fi
 
