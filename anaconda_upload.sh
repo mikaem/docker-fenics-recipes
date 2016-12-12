53b01ae03da199d1a1d92b917868765f46a64f06
Mikael Mortensen <mikaem@math.uio.no>
2016-12-12 09:54:11 +0100

    Adding slepc4py to anaconda_upload

--- anaconda_upload.sh
+++ anaconda_upload.sh
@@ -14,6 +14,7 @@ anaconda upload ${CONDA_BUILD_DIR}/eigen3-3.2.1-${CONDA_BUILD_NUMBER}.tar.bz2 --
 anaconda upload --force ${CONDA_BUILD_DIR}/petsc-3.7.3-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 anaconda upload --force ${CONDA_BUILD_DIR}/petsc4py-3.7.0-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 anaconda upload --force ${CONDA_BUILD_DIR}/slepc-3.7.2-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
+anaconda upload --force ${CONDA_BUILD_DIR}/slepc4py-3.7.0-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 
 anaconda upload ${CONDA_BUILD_DIR}/fenics-${FENICS_VERSION}-py27_${CONDA_BUILD_NUMBER}.tar.bz2 --label ${CONDA_BUILD_LABEL}
 
