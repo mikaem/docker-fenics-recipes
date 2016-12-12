d548990ced71ecb58c1166c4128c0c5cc2ba36a3
Mikael Mortensen <mikaem@math.uio.no>
2016-12-12 17:52:11 +0100

    Adding osx-host option

--- cf-fenics/host-gcc/build.sh
+++ cf-fenics/host-gcc/build.sh
@@ -2,7 +2,9 @@
 
 if [[ "$(uname)" == "Darwin" ]]; then
   export MACOSX_DEPLOYMENT_TARGET=10.9
-  export CXXFLAGS="-std=c++11 -stdlib=libc++ $CXXFLAGS -Wl,-rpath,$PREFIX/lib"
+  MACOSX_VERSION_MIN=10.9
+  export CXXFLAGS="-stdlib=libc++ -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export LDFLAGS="-stdlib=libc++ -mmacosx-version-min=${MACOSX_VERSION_MIN}  -Wl,-rpath,$PREFIX/lib"
 fi
 
 # Components (ffc, etc.)
@@ -14,6 +16,9 @@ pip install --no-deps --no-binary :all: -r "${RECIPE_DIR}/component-requirements
 # Remove it because it breaks building on Python 3.
 rm -rf dolfin/swig/modules
 
+CC=mpicc
+CXX=mpic++
+
 rm -rf build
 mkdir build
 cd build
@@ -22,6 +27,11 @@ export LIBRARY_PATH=$PREFIX/lib
 export INCLUDE_PATH=$PREFIX/include
 
 export PETSC_DIR=$PREFIX
+#export PETSC_ARCH=arch-conda-c-opt
+export PETSC_VERSION=3.7.3
+export PARMETIS_DIR=$PREFIX/lib
+export METIS_DIR=$PREFIX/lib
+export UMFPACK_DIR=$PREFIX/lib
 export SLEPC_DIR=$PREFIX
 export BLAS_DIR=$LIBRARY_PATH
 export SCOTCH_DIR=$PREFIX
@@ -34,6 +44,12 @@ cmake .. \
   -DDOLFIN_ENABLE_SCOTCH=on \
   -DDOLFIN_ENABLE_HDF5=on \
   -DDOLFIN_ENABLE_VTK=on \
+  -DCMAKE_C_COMPILER=$CC \
+  -DCMAKE_CXX_COMPILER=$CXX \
+  -DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath,$PREFIX/lib \
+  -DBLAS_LIBRARIES=$PREFIX/lib/libopenblas.dylib \
+  -DCMAKE_MACOSX_RPATH=ON \
+  -DDOLFIN_SKIP_BUILD_TESTS:BOOL=TRUE \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_INCLUDE_PATH=$INCLUDE_PATH \
   -DCMAKE_LIBRARY_PATH=$LIBRARY_PATH \
---
+++ petsc/osx-host/build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+export PETSC_DIR=$SRC_DIR
+export PETSC_ARCH=arch-conda-c-opt
+
+if [[ $(uname) == Darwin ]]; then
+    SO=dylib
+else
+    SO=so
+fi
+
+$PYTHON ./configure \
+  LDFLAGS="$LDFLAGS" \
+  --with-fc=0 \
+  --with-debugging=0 \
+  --COPTFLAGS=-O3 \
+  --CXXOPTFLAGS=-O3 \
+  --LIBS=-Wl,-rpath,$PREFIX/lib \
+  --with-blas-lapack-lib=libopenblas.$SO \
+  --with-hwloc=0 \
+  --with-mpi=1 \
+  --with-pthread=1 \
+  --with-ptscotch=1 \
+  --with-ssl=0 \
+  --with-suitesparse=1 \
+  --with-x=0 \
+  --download-hypre \
+  --download-metis \
+  --download-parmetis \
+  --with-shapred-libraries \
+  --prefix=$PREFIX
+
+sedinplace() { [[ $(uname) == Darwin ]] && sed -i "" $@ || sed -i"" $@; }
+for path in $PETSC_DIR $PREFIX; do
+    sedinplace s%$path%\${PETSC_DIR}%g $PETSC_ARCH/include/petsc*.h
+done
+
+make
+make check
+make install
+
+rm -fr $PREFIX/bin
+rm -fr $PREFIX/share
+rm -fr $PREFIX/lib/lib$PKG_NAME.*.dylib.dSYM
+rm -f  $PREFIX/lib/$PKG_NAME/conf/files
+rm -f  $PREFIX/lib/$PKG_NAME/conf/*.py
+rm -f  $PREFIX/lib/$PKG_NAME/conf/*.log
+rm -f  $PREFIX/lib/$PKG_NAME/conf/RDict.db
+rm -f  $PREFIX/lib/$PKG_NAME/conf/*BuildInternal.cmake
+find   $PREFIX/include -name '*.html' -delete
