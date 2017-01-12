5f5c66050d892ff5a917ba7c82721068717917e0
sigvaldm <sigvaldm@fys.uio.no>
2017-01-12 14:38:45 +0100

    Fixed syntax mistake in build_fenics_deps.sh and requires build_fenics.conf to be sourced

--- build_fenics_deps.sh
+++ build_fenics_deps.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-if [ ${CONDA_BUILD_TYPE} = host-gcc ]; then
+if [ "${CONDA_BUILD_TYPE}" = "host-gcc" ]; then
     # Dependencies
-#     conda build subprocess32/host-gcc
-#     conda build eigen3/host-gcc
-#     conda build boost/host-gcc
-#     conda build hdf5-parallel/host-gcc
-#     conda build h5py-parallel/host-gcc
-#     conda build vtk/host-gcc
+    conda build subprocess32/host-gcc
+    conda build eigen3/host-gcc
+    conda build boost/host-gcc
+    conda build hdf5-parallel/host-gcc
+    conda build h5py-parallel/host-gcc
+    conda build vtk/host-gcc
     conda build petsc/host-gcc
-#     conda build petsc4py/host-gcc
-#     conda build slepc/host-gcc
+    conda build petsc4py/host-gcc
+    conda build slepc/host-gcc
 
-elif [ ${CONDA_BUILD_TYPE} = conda-gcc ]; then
+elif [ "${CONDA_BUILD_TYPE}" = "conda-gcc" ]; then
     # Cannot seem to build VTK with conda gcc
     conda build hdf5-parallel/conda-gcc
     conda build h5py-parallel/conda-gcc
@@ -20,7 +20,8 @@ elif [ ${CONDA_BUILD_TYPE} = conda-gcc ]; then
     conda build petsc4py/conda-gcc
     conda build slepc/conda-gcc
 
-else
+elif [ "${CONDA_BUILD_TYPE}" = "osx-gcc" ]; then
+
     conda config --add channels conda-forge
     #conda build hdf5-parallel/osx-host
     #conda build h5py-parallel/osx-host
@@ -30,5 +31,8 @@ else
     conda build slepc4py/osx-host
     #conda build vtk/osx-host
 
-fi
+else
 
+	echo "Configure build_fenics.conf and source it before executing this"
+
+fi
