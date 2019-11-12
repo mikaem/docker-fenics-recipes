96797f440cf00b4765ba14580a37cd98ae7eb00e
Mikael Mortensen <mikaem@math.uio.no>
2019-11-12 11:09:31 +0100

    Adding more libnetcdf files

---
+++ libnetcdf-parallel/linux/run_test.sh
@@ -0,0 +1,8 @@
+nc-config --has-dap      | grep -q yes
+nc-config --has-dap2     | grep -q yes
+nc-config --has-dap4     | grep -q yes
+nc-config --has-nc2      | grep -q yes
+nc-config --has-nc4      | grep -q yes
+nc-config --has-hdf5     | grep -q yes
+nc-config --has-logging  | grep -q yes
+nc-config --has-cdf5     | grep -q yes
